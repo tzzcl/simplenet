0003a224f8d466bf6d7aeebb7b967a2c2257f5c4
formalhhh <formalhhh@qq.com>
2015-05-11 07:33:53 -0700

    few

--- common/seg.c
+++ common/seg.c
@@ -180,8 +180,8 @@ bad:
 int forwardsegToSTCP(int stcp_conn, int src_nodeID, seg_t* segPtr)
 {
 	sendseg_arg_t *seg_arg = malloc(sizeof(sendseg_arg_t));
-	if (send(sip_conn,"!&",2,0)<=0) goto bad;
-	if (send(sip_conn,&src_nodeID,sizeof(int),0)<=0) goto bad;
+	if (send(stcp_conn,"!&",2,0)<=0) goto bad;
+	if (send(stcp_conn,&src_nodeID,sizeof(int),0)<=0) goto bad;
 	stcp_hdr_t *header=&(segPtr->header);
 	if (send(sip_conn,segPtr,HEADER_LENGTH+header->length,0)<=0) goto bad;
 	printf("%s: succeeded\n",__FUNCTION__);
